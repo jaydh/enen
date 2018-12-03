a6c0be940b38131f6cace07af294d731d93b588a
Jay Howard <jayhoward300@gmail.com>
2018-12-02 21:12:36 -0800

    Add visiblity dom eventhandler to trigger clipboard reading

--- src/components/AddArticleForm.tsx
+++ src/components/AddArticleForm.tsx
@@ -29,19 +29,15 @@ class AddArticleForm extends React.Component<IProps, IState> {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.getValidationState = this.getValidationState.bind(this);
+    this.readClipboard = this.readClipboard.bind(this);
   }
 
   public componentDidMount() {
-    // Copy from clipboard if valid article
-    if ((navigator as any).clipboard.readText) {
-      (navigator as any).clipboard
-        .readText()
-        .then((text: any) =>
-          this.getValidationState(text)
-            ? this.setState({ value: text, valid: true })
-            : Promise.resolve()
-        );
-    }
+    document.addEventListener('visibilitychange', this.readClipboard, false);
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener('visibilitychange', this.readClipboard, false);
   }
 
   public render() {
@@ -88,6 +84,20 @@ class AddArticleForm extends React.Component<IProps, IState> {
     const exists = this.props.articles[hash(value)];
     // Checks if valid hyperlink
     return !exists && parse.authority && parse.protocol ? true : false;
+  }
+
+  private readClipboard() {
+    // Copy from clipboard if valid article
+    if (!document.hidden && (navigator as any).clipboard.readText) {
+      (navigator as any).clipboard
+        .readText()
+        .then(
+          (text: any) =>
+            this.getValidationState(text)
+              ? this.setState({ value: text, valid: true })
+              : Promise.resolve()
+        );
+    }
   }
 }
 
