34152c11e67a63ea41460db1c4b61c5ceaa67ed5
Jay Howard <jayhoward300@gmail.com>
2018-12-04 17:29:50 -0800

    Testing: date completion graph

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,3 +1,11 @@
+import {
+  isSameDay,
+  isSunday,
+  isWithinRange,
+  parse,
+  subDays,
+  subWeeks
+} from 'date-fns';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme } from 'victory';
@@ -8,15 +16,30 @@ import { VictoryAxis, VictoryBar, VictoryChart, VictoryTheme } from 'victory';
 const { VictoryLegend } = require('victory');
 
 interface IProps {
+  completionDates: any;
   domains: {};
 }
 
 class Graph extends React.Component<IProps> {
   public render() {
-    const { domains } = this.props;
+    const { domains, completionDates } = this.props;
     const data = Object.keys(domains).map((key: string) => {
       return { domain: key, count: domains[key] };
     });
+    const today = new Date();
+    let runner = new Date();
+    const dates = {};
+    while (!isSameDay(runner, subWeeks(today, 4))) {
+      dates[runner.toDateString()] = 0;
+      runner = subDays(runner, 1);
+    }
+    // tslint:disable:no-console
+    const inRange = completionDates
+      .filter((t: Date) => isWithinRange(t, subWeeks(today, 4), today))
+      .forEach((t: Date) => {
+        dates[t.toDateString()] = dates[t.toDateString()] + 1;
+      });
+    console.log(inRange, dates);
 
     return (
       <>
@@ -35,13 +58,40 @@ class Graph extends React.Component<IProps> {
           theme={VictoryTheme.material}
           padding={{ left: 200, top: 50, right: 10, bottom: 50 }}
         >
-          <VictoryAxis dependentAxis={true} tickFormat={Object.keys(domains)} />
+          <VictoryAxis dependentAxis={true} tickValues={Object.keys(domains)} />
           <VictoryBar
             labels={(d: any) => d.count}
             horizontal={true}
             data={data}
             y="count"
             x="domain"
+            style={{
+              data: {
+                fillOpacity: 0.7,
+                strokeWidth: 3
+              },
+              labels: {
+                fontSize: 15
+              }
+            }}
+          />
+        </VictoryChart>
+        <VictoryChart
+          animate={{ duration: 500, easing: 'bounce' }}
+          theme={VictoryTheme.material}
+          padding={{ left: 200, top: 50, right: 10, bottom: 50 }}
+        >
+          <VictoryAxis
+            dependentAxis={false}
+            tickValues={Object.keys(dates).filter((t: string) =>
+              isSunday(parse(t))
+            )}
+            style={{
+              tickLabels: { fontSize: 20, padding: 30, angle: 45 } as any
+            }}
+          />
+          <VictoryBar
+            data={Object.keys(dates).map((t: string) => dates[t])}
             style={{
               data: {
                 fillOpacity: 0.7,
@@ -60,6 +110,9 @@ class Graph extends React.Component<IProps> {
 
 const mapStateToProps = (state: any) => {
   return {
+    completionDates: state.articles.articles.map((t: any) =>
+      parse(t.completedOn)
+    ),
     domains: state.graphData.domains
   };
 };
